3888733990f1502ec4775c9bb08196b503487312
srijesh158 <reddysrijesh6@gmail.com>
2025-01-29 19:58:48 +0530

    adding files

---
+++ assessment.js
@@ -0,0 +1,32 @@
+const employees = [
+    {
+      EMP_ID: 1,
+      EMP_NAME: 'Manoj',
+      EMP_COMPANY: 'Accenture',
+      EMP_SALARY: 60000,
+      EMP_ADDRESS: {
+        EMP_CITY: 'HYDERABAD',
+        EMP_AREA: 'HYDERABAD'
+      }
+    },
+    {
+      EMP_ID: 2,
+      EMP_NAME: 'Srijesh',
+      EMP_COMPANY: 'Wipro',
+      EMP_SALARY: 75000,
+      EMP_ADDRESS: {
+        EMP_CITY: 'HYDERABAD',
+        EMP_AREA: 'HYDERABAD'
+      }
+    },
+    {
+      EMP_ID: 3,
+      EMP_NAME: 'Chaitanya',
+      EMP_COMPANY: 'HCL',
+      EMP_SALARY: 150000,
+      EMP_ADDRESS: {
+        EMP_CITY: 'HYDERABAD',
+        EMP_AREA: 'HYDERABAD'
+      }
+    }
+  ];
