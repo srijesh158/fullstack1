328b38e7e1398a2e1dd5027243052547e5835b07
srijesh158 <reddysrijesh6@gmail.com>
2025-01-31 11:53:55 +0530

    adding files

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
-var a= 10;
-console.log(a);
-// let str1 = "double quotes";
-// console.log(str1 , typeof(str1));
+//import x from "./child1.js"
+ //console.log(x);
 
-// let str2 ='single quotes';
-// Console.log(str2 , typeof(str2));
-
-// let str3 = `template literls`;
-// Console.log(str3 , typeof(str3));
-
-// let bool1 = true;
-// Console.log(bool1 , typeof(bool1));
-
-// let bool2 = false:
-// Console.log(bool2 , typeof(bool2));
-
-// let num1 = 10;
-// console.log(num1 , typeof(num1));
-
-// let num2 = 10.432;
-// console.log(num2 , typeof(num2));
-
-// let num3 = 10e3;
-// console.log(num3 , typeof(num3));
-
-// let a;
-//console.log(a , typeof(a));
-
-//let x = null;
-//console.log(x , typeof(x));
-
+ // !named import
+import college ,{x, user, users, obj} from "./child1.js";
+ console.log(college);
+ console.log(x);
+ console.log(user);
+ console.log(users);
+ users.map(user=>{
+    console.log(user);
+ })
+ console.log(obj.company);
---
+++ child1.js
@@ -0,0 +1,35 @@
+// let x = 10;
+
+// export default x;
+
+// let userdetail={
+//     name:"sathvik",
+//     profession:"cr",
+// }
+// export default userdetail;
+
+// ! named export
+// let x = 10;
+// const user ="Srijesh";
+// const users=["nithin" , "manoj" , "manohar" , "bhargav"]
+// const obj={
+//     name:"sam",
+//     company:"excelr"
+// }
+
+// export{
+//     x,
+//     user,
+//     users,
+//     obj
+// }
+
+export let x = 10;
+export const user="srijesh";
+export const users=["nithin" , "manoj" , "manohar" , "bhargav"]
+export const obj={
+    name:"sam",
+    company:"excelr"
+}
+let college="Mallareddy University";
+export default college;
